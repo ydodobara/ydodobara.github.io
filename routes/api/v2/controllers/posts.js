2ac6878c1e3f3d3066a6b82899928518ccae7bc8
ydodobara <ydodo@uw.edu>
2024-04-16 23:43:31 -0700

    finished posts.js

--- routes/api/v2/controllers/posts.js
+++ routes/api/v2/controllers/posts.js
@@ -4,6 +4,48 @@ var router = express.Router();
 
 import getURLPreview from '../utils/urlPreviews.js';
 
-//TODO: Add handlers here
+router.post('/', async (req, res) => {
+
+    try {
+        const newPost = new req.models.Post({
+            url: req.body.url,
+            description: req.body.description,
+            created_date: newDate()
+        });
+
+        await newPost.save();
+
+        res.send("Success");
+
+        res.status(500).json({"status": "success"});
+
+    }
+    catch(err) {
+        console.log("Error connecting to db", err);
+        res.status(500).json({"status": "error", "error": error});
+    }
+});
+
+router.get('/', async function(req, res, next) {
+    try{
+      let allPosts = await req.models.Post.find()
+      let postData = await Promise.all(
+        allPosts.map(async post => {
+            try {
+                let {description, url} = post;
+                let htmlPreview = await getURLPreview(url);
+                return {description: description, htmlPreview: htmlPreview};
+            }
+            catch(error) {
+                console.log("Error:", error);
+                return {description: "error", htmlPreview: error}
+            }
+        })
+      )
+    } catch(error){
+      console.log("Error:", error);
+      res.status(500).json({"status": "error", "error": error});
+    }
+  });
 
 export default router;
--- routes/api/v2/controllers/urls.js
+++ routes/api/v2/controllers/urls.js
@@ -4,15 +4,14 @@ var router = express.Router();
 
 import getURLPreview from '../utils/urlPreviews.js';
 
-router.post('/preview', async (req, res) => {
-
+router.get('/preview', async (req, res) => {
     try {
         getURLPreview(req.query.url);
         res.send("Success");
     }
     catch(err) {
-        console.log("Error connecting to db", err);
-        res.status(500).json({"status": "error", "error": err});
+        console.log("Error getting preview of url", err);
+        res.status(500).send("Error getting preview of url");
     }
 });
 
